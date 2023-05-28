1e0cc10a4acc092472dc51f3fc1cdbff83d70210
COLDash0506 <naonlcj0506@gmail.com>
2023-05-29 01:48:53 +0900

    Ibid.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# 2023-Educational-Dart-Practice
+# Dart-Practice
 간단한 다트 예제 연습용 레포지토리 입니다
 연습 폴더 아래에 각자 자신의 이름을 적은 폴더를 생성하고 그 위에 실습한 코드를 저장하면 됩니다.
 실습을 마친 후에는 Github Desktop의 Commit과 publish를 실행하면 됩니다.
-*컴파일:F5
 강의 링크: https://nomadcoders.co/dart-for-beginners
 여기서 코드 활동한 내역은 자기 자신의 세부특기사항이 될 수 있으니
 열심히 참여 부탁드립니다 ^__^
+ 
--- 연습/임찬재/ex1.dart
+++ 연습/임찬재/ex1.dart
@@ -1,3 +1,26 @@
-void main() {
-  print("Siuuuuu");
+void main(){
+  print('hello world');
+  //모든 프로그램의 시작은 void main부터
+  
+  var name;
+  name = 1;
+  name = 'me';
+  name = true;
+  name = false;
+
+  String name1 = 'you';
+  //var는 변수, 함수, 메소드 내부에서 지역 변수를 선언할때 주로 사용하게 된다.
+  //class 내에서 변수나 'property' 선언 시에는 주로 타입(String 등등-첫글자는 대문자 부터)을 지정하게 된다.
+  //var로 선언하는 변수는 기본적으로 dynamic 타입이다.(따로따로 선언-지정시.)
+  
+  var name0;
+  if(name is String){
+    name0.hashCode;
+  }
+  if(name0 is num){
+    name0.isInfinite;
+  }
+  //dynamic은 적재적소에만 활용하자.(남용 금지.)
+
+  
 }
